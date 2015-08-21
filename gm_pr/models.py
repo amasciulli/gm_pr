8b528e75dbd4a7dbb463903c27d273ba160917f2
Carmen Alvarez <c@rmen.ca>
2015-08-21 21:41:57 +0200

    Issue #43: add a column for the lastest activity in a PR

--- gm_pr/models.py
+++ gm_pr/models.py
@@ -16,7 +16,7 @@
 class Pr:
     """ Simple class wrapper for pr properties
     """
-    def __init__(self, url="", title="", updated_at="", user="",
+    def __init__(self, url="", title="", updated_at="", user="", last_activity=None,
                  repo="", nbreview=0, feedback_ok=0, feedback_weak=0,
                  feedback_ko=0, milestone=None, labels=None,
                  is_old=False):
@@ -24,6 +24,7 @@ class Pr:
         self.title = title
         self.updated_at = updated_at
         self.user = user
+        self.last_activity = last_activity
         self.repo = repo
         self.nbreview = nbreview
         self.feedback_ok = feedback_ok
